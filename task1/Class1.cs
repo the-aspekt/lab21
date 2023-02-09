564d492894477666f21f61cca78acdaf162d84c1
Pavel <sozonovps@gmail.com>
2023-02-09 21:51:59 +0800

    Добавьте файлы проекта.

---
+++ task1/Class1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    internal class Garden
+    {
+        const int scale = 10;
+        public int Scale { get; set; }
+
+        string[,] place = new string[scale, scale];
+        public void ChangePlace(int i, int j, string place)
+        {
+            this.place[i, j] = place;
+        }
+
+        public string GetPlace(int i, int j)
+        {
+            return this.place[i, j];
+        }
+
+        public Garden()
+        {
+            Scale = scale;
+            for (int i = 0; i < scale; i++)
+            {
+                for (int j = 0; j < scale; j++)
+                {
+                    ChangePlace(i, j, "0");
+                }
+            }
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < scale; i++)
+            {
+                for (int j = 0; j < scale; j++)
+                {
+                    Console.Write(GetPlace(i, j) + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public bool Finished()
+        {
+            int counter = 0;
+            for (int i = 0; i < scale; i++)
+            {
+                for (int j = 0; j < scale; j++)
+                {
+                    if (GetPlace(i, j) != "0")
+                        counter++;
+                }
+            }
+            if (counter == scale * scale)
+                return true;
+            else return false;
+        }
+    }
+
+    internal class Gardener
+    {
+        int speed;
+        public int Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+        char symbol;
+        bool directionType;
+        public int[] position = {0,0};
+        int scale;
+
+        public void Move()
+        {
+            if (!directionType)
+            {
+                if (position[0] + 1 <= scale)
+                    position[0] = position[0] + 1;
+                else if (position[1] + 1 <= scale)
+                {
+                    position[0] = 0;
+                    position[1] = position[1] + 1;
+                }
+            }
+            else 
+            { 
+                if (position[1] - 1 >= 0)
+                    position[1] = position[1] - 1;
+                else if (position[0] - 1 >= 0)
+                {
+                position[1] = scale;
+                position[0] = position[0] - 1;
+                }
+            }
+        }
+
+        public Gardener(Garden garden, int speed, char symbol = 'X', bool directionType = false)
+        {
+            Speed = speed;
+            if (symbol == '0')
+            {
+                this.symbol = 'X';
+            }
+            else this.symbol = symbol;
+
+            this.directionType = directionType;
+            scale = garden.Scale - 1;
+
+            position[0] = directionType ? scale : 0;
+            position[1] = directionType ? scale : 0;
+            
+
+        }
+
+        public string Perform() => Convert.ToString(symbol);
+
+    }
+
+}
---
+++ task1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Два садовника оформляют сад.");
+            Garden garden = new Garden();
+            garden.Print();
+            Console.WriteLine("Задайте скорость первого садовника целым числом от 1 до 10");
+            int s1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Задайте символ, в виде которого будет делать кусты первый садовник");
+            char c1 = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Отлично! Теперь то же самое, но для второго садовника");
+            Console.WriteLine("Задайте скорость второго садовника целым числом от 1 до 10");
+            int s2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Задайте символ, в виде которого будет делать кусты второй садовник");
+            char c2 = Convert.ToChar(Console.ReadLine());
+
+            Gardener gardener1 = new Gardener(garden,s1,c1);
+            Gardener gardener2 = new Gardener(garden, s2, c2, true);
+
+
+            void Plant(Garden currentGarden, Gardener currentGardener)
+            {
+                while (!currentGarden.Finished())
+                {
+                    if (currentGarden.GetPlace(currentGardener.position[0], currentGardener.position[1]) == "0")
+                    {
+                        currentGarden.ChangePlace(currentGardener.position[0], currentGardener.position[1], currentGardener.Perform());
+                        currentGardener.Move();
+                        Thread.Sleep(currentGardener.Speed*100);
+                    }
+                    else
+                    {
+                        currentGardener.Move();
+                    }
+                }
+            }
+
+            void Plant1()
+            {
+                Plant(garden, gardener1);
+            }
+            void Plant2()
+            {
+                Plant(garden, gardener2);
+            }
+            ThreadStart threadStart1 = new ThreadStart(Plant1);
+            ThreadStart threadStart2 = new ThreadStart(Plant2);
+            Thread thread1 = new Thread(threadStart1);
+            Thread thread2 = new Thread(threadStart2);
+            thread1.Start();
+            thread2.Start();
+
+            while (!garden.Finished())
+            {
+                Console.Clear();
+                garden.Print();
+                Thread.Sleep(10);
+            }
+
+            Console.WriteLine("Работа над садом закончена!");
+            Console.ReadKey();
+
+        }
+
+
+
+    }
+
+   
+
+
+    }
+
+
